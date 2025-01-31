b321cbddd053a9ae457b9f5fae3cdbea125c7416
ARK-JUTT <ahmad236997@gmail.com>
2025-01-31 22:54:02 +0500

    final capstone

--- Blog web app/index.js
+++ Blog web app/index.js
@@ -12,20 +12,55 @@ app.use(express.static("public"));
 
 let posts = [];
 
+let id = 0;
+
 app.get("/", (req, res) => {
     res.render("index.ejs", {posts: posts});
 });
+
+function createBlogPreview(blogBody, maxWords = 8){
+    const preview = blogBody.split(' ').slice(0, maxWords).join(' ');
+    return preview;
+}
+
+app.get("/posts/:id", (req, res) => {
+    const postId = parseInt(req.params.id);
+    const post = posts.find(p => p.id === postId);
+
+    if(post){
+        res.render("show.ejs",{post});
+        console.log(`Thats what you are looking for ${post}`);
+    } else {
+        res.status(404).send("Post not found");
+    }
+});
+
+app.delete("/posts/:id", (req, res) => {
+    const postId = parseInt(req.params.id);
+    const postIndex = posts.findIndex(post => post.id === postId);
+
+    if(postIndex !== -1){
+        posts.splice(postIndex, 1);
+        console.log(`The post with id ${postId} has been deleted`);
+        res.redirect("/")
+    } else {
+        res.status(404).send("Post not found");
+    }
+
+})
 
 app.post("/", (req, res) => {
     const blogName = req.body.blogName;
     const blogBody = req.body.blogBody;
     if(blogName.trim() != "" && blogBody.trim() != ""){
-        posts.push({name: blogName, body: blogBody});
+        const previewBlog = createBlogPreview(blogBody);
+        id++;
+        posts.push({name: blogName,preview: previewBlog ,body: blogBody, id: id});
         console.log(posts)
         // res.render("index.ejs", {posts: posts});
     }
     else{
-        console.log("Naaah")
+        return res.redirect("/?error=Please fill in all fields");
     }
     res.redirect("/");
 });
